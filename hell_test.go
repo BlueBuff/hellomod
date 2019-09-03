ab5b372f55a47a6a5632b032fb159c2319cc7843
huangxiaojun <1010060164@qq.com>
2019-09-03 15:53:54 +0800

    v3

--- hell_test.go
+++ hell_test.go
@@ -3,7 +3,7 @@ package hellomod
 import "testing"
 
 func TestHello(t *testing.T){
-	want := "v2:Hello World!"
+	want := "v3:Hello World!"
 	if Hello() != want {
 		t.Errorf("Hello() != %s",want)
 	}
